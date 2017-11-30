040700be434e80da907c36f1944b7a8adab9be43
dariate <33526136+DariaTrueTest@users.noreply.github.com>
2017-11-30 18:25:15 +0200

    Add files via upload

---
+++ GarageCoop-master/team proj/Forms/DirectorWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace team_proj.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для DirectorWindow.xaml
+    /// </summary>
+    public partial class DirectorWindow : Window
+    {
+        public DirectorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void butCalc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butChange_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
